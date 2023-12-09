e912669bdb6c7584e682a20ec708abf2eece25c4
alekospagon <56389540+alekospagon@users.noreply.github.com>
2023-12-09 02:03:52 +0200

    Update and rename day8_part1.hs to day8_bothparts.hs

--- 2023/day8_bothparts.hs
+++ 2023/day8_bothparts.hs
@@ -1,21 +1,27 @@
-{-# LANGUAGE BangPatterns #-}
 import Data.List
 import Data.List.Split
 import Data.Map (fromList, (!))
 
 
-count "ZZZ" !cnt my_map (i:instructions) = cnt
-count  key  !cnt my_map (i:instructions) =
+beg s = 'A' == last s
+end s = 'Z' == last s
+
+-- solve for one key on map
+count key cnt my_map (i:instructions) =
+        if end key then cnt else
         if i == 'L'
                 then count left  (1+cnt) my_map instructions
                 else count right (1+cnt) my_map instructions
         where (left, right) = my_map ! key
 
 
-
+-- part 2 --> just LCM of all
 main = do
         instructions <- getLine
         _ <- getLine -- consume empty line
         rest <- getContents
         let my_map = fromList [(e!!0, (e!!1,e!!2)) | e<- map (words . filter (\e -> not (elem e ",()="))) $ lines rest]
+        let starting_points = filter beg $ map (head . words) $ lines rest
+        -- part 1 and 2
         print $ count "AAA" 0 my_map (cycle instructions)
+        print $ foldr lcm 1 $ map (\x -> count x 0 my_map (cycle instructions)) starting_points
